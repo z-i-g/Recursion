cfaaf741c4205147328763595564772d2c618d36
Zagidullin Ayrat <zagidull007@gmail.ru>
2023-08-03 20:36:02 +0300

    ListEvenValuesPrinting - поправил логику без изменения и копирования списка

--- src/main/java/ListEvenValuesPrintingService.java
+++ src/main/java/ListEvenValuesPrintingService.java
@@ -1,12 +1,19 @@
 import java.util.List;
 
 public class ListEvenValuesPrintingService {
+
     public static void execute(List<Integer> list) {
+        executeRecursively(list, 0);
+    }
+
+    private static void executeRecursively(List<Integer> list, int startIndex) {
         if (list == null || list.isEmpty())
             return;
-        if (list.get(0) % 2 == 0)
-            System.out.print(list.get(0) + " ");
-        list.remove(0);
-        execute(list);
+        if (list.size() <= startIndex)
+            return;
+        if (list.get(startIndex) % 2 == 0)
+            System.out.print(list.get(startIndex) + " ");
+        startIndex++;
+        executeRecursively(list, startIndex);
     }
 }
