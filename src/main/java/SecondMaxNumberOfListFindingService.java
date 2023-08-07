c0ffe144fc18b535f69ae6b22582744a4269c0a5
Zagidullin Ayrat <zagidull007@gmail.ru>
2023-08-07 22:28:55 +0300

    SecondMaxNumberOfListFindingService - убрал закомментированный код

--- src/main/java/SecondMaxNumberOfListFindingService.java
+++ src/main/java/SecondMaxNumberOfListFindingService.java
@@ -25,31 +25,4 @@ public class SecondMaxNumberOfListFindingService {
 
         return executeRecursively(list, maxElement, secondMaxElement, iterateIndex + 1);
     }
-
-//    private static int executeRecursively(List<Integer> list, int maxElementIndex, int secondMaxElementIndex, int iterateIndex) {
-//        if (list == null || list.isEmpty() || list.size() == 1)
-//            throw new RuntimeException("List is empty or null or contains one element");
-//
-//        if (list.size() == 2)
-//            return Math.min(list.get(maxElementIndex), list.get(secondMaxElementIndex));
-//
-//        if (iterateIndex + 1 >= list.size())
-//            return Math.min(list.get(maxElementIndex), list.get(secondMaxElementIndex));
-//
-//        if (list.get(maxElementIndex) < list.get(secondMaxElementIndex)) {
-//            maxElementIndex = secondMaxElementIndex;
-//            secondMaxElementIndex--;
-//        }
-//
-//        if (list.get(maxElementIndex) <= list.get(iterateIndex + 1)) {
-//            secondMaxElementIndex = maxElementIndex;
-//            maxElementIndex = iterateIndex + 1;
-//        }
-//
-//        if (list.get(secondMaxElementIndex) <= list.get(iterateIndex + 1)) {
-//            secondMaxElementIndex = iterateIndex + 1;
-//        }
-//
-//        return executeRecursively(list, maxElementIndex, secondMaxElementIndex, iterateIndex + 1);
-//    }
 }
