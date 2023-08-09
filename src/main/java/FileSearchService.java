59440d09458a201d8761e2c0990590b4a759e708
Zagidullin Ayrat <zagidull007@gmail.ru>
2023-08-09 11:35:46 +0300

    FileSearchService - убрал закомментированный код

--- src/main/java/FileSearchService.java
+++ src/main/java/FileSearchService.java
@@ -26,25 +26,4 @@ public class FileSearchService {
         }
         return filesList;
     }
-
-//    public static List<String> execute(String directoryPath) {
-//        if (directoryPath == null || directoryPath.isEmpty())
-//            throw new IllegalArgumentException("The parameter is null or empty");
-//
-//        File directory = new File(directoryPath);
-//        if (directory.isFile())
-//            throw new IllegalArgumentException("The parameter must be the address of the directory");
-//        return executeRecursively(directory, new ArrayList<>());
-//    }
-//
-//    private static List<String> executeRecursively(File directory, List<String> filesList) {
-//        for (File currentFile : Objects.requireNonNull(directory.listFiles())) {
-//            if (currentFile.isFile())
-//                filesList.add(currentFile.getName());
-//
-//            if (currentFile.isDirectory())
-//                executeRecursively(currentFile, filesList);
-//        }
-//        return filesList;
-//    }
 }
